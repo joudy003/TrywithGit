9f5df2438ececd2a6b119b4880dbf1a82d38243d
joudy003 <joudy.j.t@gmail.com>
2024-09-26 16:47:24 +0300

    Initial Commit

---
+++ TrywithGit/TrywithGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrywithGitApp.swift
+//  TrywithGit
+//
+//  Created by Joud Abdullah AlAmri on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TrywithGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
